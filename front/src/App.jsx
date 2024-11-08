5d6777db1ce72736859b9306dd0e310f15285111
Daniel <daniel.cermann@gmx.de>
2024-11-08 21:13:15 +0100

    fix tailwind and stuff

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,14 @@
-import Editor from './pages/Editor'
-import ModelViewer from './pages/ModelViewer'
+// import Editor from './pages/Editor'
+// import ModelViewer from './pages/ModelViewer/ModelViewer'
 
 function App() {
-  return <Editor />
+  return (
+  <div className='h-full bg-red-50'>
+    Penis
+    {/* <ModelViewer></ModelViewer> */}
+
+    {/* <Editor /> */}
+  </div>)
 }
 
 export default App
