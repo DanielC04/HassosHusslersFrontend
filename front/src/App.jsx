03025fe630a6c045048b37803303bd156ebc53be
jafber <spam@jan-berndt.de>
2024-11-09 10:23:16 +0100

    reset accidentally changed files

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,7 @@
 // import Editor from './pages/Editor'
 // import ModelViewer from './pages/ModelViewer/ModelViewer'
-import LineEditor from './components/LineEditor'
 
 function App() {
-  return <LineEditor />
   return (
   <div className='h-full bg-red-50'>
     Penis
