eafda5addde993c825fab17fd69a228193382157
Daniel <daniel.cermann@gmx.de>
2024-11-09 10:56:55 +0100

    finished merge

--- front/src/pages/ModelViewer/ModelViewer.jsx
+++ front/src/pages/ModelViewer/ModelViewer.jsx
@@ -1,21 +1,14 @@
 import React, { useState } from 'react'
 import { Canvas  } from '@react-three/fiber'
-<<<<<<< HEAD
-import { OrbitControls } from '@react-three/drei'
-=======
 import { OrbitControls, MapControls } from '@react-three/drei'
->>>>>>> 6e5cc3f (rotated world by 90deg)
 import { Ground } from './components/Ground'
 import WallStrip from './components/WallStrip'
 import Elevator from './components/Elevator'
 import Shaft from './components/Shaft'
 import { DraggableInstance } from './components/DraggableInstance'
 
-<<<<<<< HEAD
-=======
 const WORLD_SIZE = 40
 
->>>>>>> 6e5cc3f (rotated world by 90deg)
 export default function ModelViewer(props) {
     const wallStrip = [[0, 0], [8, 0], [12, 3], [8, 5], [0, 5]]
     const [isCameraControlActive, setCameraControlActive] = useState(true);
@@ -25,18 +18,6 @@ export default function ModelViewer(props) {
         <div className='w-full h-screen'>
             <Canvas>
                 <ambientLight intensity={Math.PI / 2} />
-<<<<<<< HEAD
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-
-                <WallStrip data={wallStrip} width={0.5} height={4} closeStrip={true}></WallStrip>
-                <Elevator setCameraActive={setCameraControlActive}></Elevator>
-                <DraggableInstance setCameraActive={setCameraControlActive} >
-                    <Shaft setCameraActive={setCameraControlActive}></Shaft>
-                </DraggableInstance>
-
-                <OrbitControls
-=======
                 <spotLight position={[WORLD_SIZE/2, WORLD_SIZE/2, WORLD_SIZE/2]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
                 <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
 
@@ -54,7 +35,6 @@ export default function ModelViewer(props) {
                     zoom0={0.10}
                  />
                 {/* <OrbitControls
->>>>>>> 6e5cc3f (rotated world by 90deg)
                     enableDamping={true}
                     dampingFactor={0.05}
                     enableZoom={true}
@@ -62,19 +42,12 @@ export default function ModelViewer(props) {
                     autoRotate={false}
                     autoRotateSpeed={2.0}
                     // maxPolarAngle={Math.PI / 2}
-<<<<<<< HEAD
-                    minDistance={2}
-                    maxDistance={50}
-                    enabled={isCameraControlActive}/>
-                <Ground></Ground>
-=======
                     minAzimuthAngle={-Math.PI / 4}    // Limits left horizontal rotation to -45 degrees
                     maxAzimuthAngle={Math.PI / 4}     // Limits right horizontal rotation to 45 degrees
                     minDistance={2}
                     maxDistance={50}
                     enabled={isCameraControlActive}/> */}
                 <Ground width={WORLD_SIZE} height={WORLD_SIZE}></Ground>
->>>>>>> 6e5cc3f (rotated world by 90deg)
             </Canvas>
         </div>
     )
